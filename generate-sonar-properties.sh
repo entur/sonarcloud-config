e882602bce990a33a33180b18e3b0a89858b27b6
pavlo rosiuk <pavlo.rosiuk@entur.org>
2020-03-24 17:08:09 +0100

    initial

---
+++ generate-sonar-properties.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Exit script if you try to use an uninitialized variable.
+set -o nounset
+set -o pipefail
+
+cat > ./sonar-project.properties <<EOL
+sonar.projectName=$(echo ${CIRCLE_PROJECT_REPONAME})
+sonar.projectKey=enturas_$(echo ${CIRCLE_PROJECT_REPONAME})
+sonar.organization=$(echo ${SONAR_ORG})
+sonar.host.url=https://sonarcloud.io
+sonar.login=$(echo ${ENTUR_SONAR_PASSWORD})
+sonar.sources=.
+sonar.branch.name=$(echo ${CIRCLE_BRANCH})
+sonar.java.binaries=$(echo ${JAVA_HOME})/bin
+EOL
